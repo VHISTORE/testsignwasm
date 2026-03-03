f2be8ff3de9dcc8f140f7f81b72cf5e4db1c6dc0
VHISTORE <vibemusic1712@gmail.com>
2026-03-03 23:13:45 +0200

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,40 @@
+// Импортируем движок внутри воркера
+importScripts('zsign.js');
+
+self.onmessage = async function(e) {
+    const { ipaData, p12Data, provData, password } = e.data;
+
+    try {
+        self.postMessage({ type: 'status', msg: '1/4 Initializing WASM Engine in background...' });
+        const zsignModule = await createZSignModule();
+
+        self.postMessage({ type: 'status', msg: '2/4 Loading files to Virtual FS...' });
+        zsignModule.FS.writeFile('app.ipa', new Uint8Array(ipaData));
+        zsignModule.FS.writeFile('cert.p12', new Uint8Array(p12Data));
+        zsignModule.FS.writeFile('prov.mobileprovision', new Uint8Array(provData));
+
+        self.postMessage({ type: 'status', msg: '3/4 Signing (CPU is working hard)...' });
+        const args = ['-k', 'cert.p12', '-p', password, '-m', 'prov.mobileprovision', '-o', 'signed.ipa', 'app.ipa'];
+        
+        // Запуск C++ движка
+        zsignModule.callMain(args);
+
+        self.postMessage({ type: 'status', msg: '4/4 Extracting signed file...' });
+        const signedIpaData = zsignModule.FS.readFile('signed.ipa');
+
+        // Отправляем готовый файл обратно в главный поток
+        self.postMessage({
+            type: 'done',
+            data: signedIpaData.buffer
+        }, [signedIpaData.buffer]);
+
+        // Очищаем виртуалку
+        zsignModule.FS.unlink('app.ipa');
+        zsignModule.FS.unlink('cert.p12');
+        zsignModule.FS.unlink('prov.mobileprovision');
+        zsignModule.FS.unlink('signed.ipa');
+
+    } catch (error) {
+        self.postMessage({ type: 'error', msg: error.toString() });
+    }
+};

a55af6ab052920f73698a269b695ea0f5d62092e
VHISTORE <vibemusic1712@gmail.com>
2026-03-03 23:19:29 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,10 @@
-// Импортируем движок внутри воркера
 importScripts('zsign.js');
 
 self.onmessage = async function(e) {
     const { ipaData, p12Data, provData, password } = e.data;
 
     try {
-        self.postMessage({ type: 'status', msg: '1/4 Initializing WASM Engine in background...' });
+        self.postMessage({ type: 'status', msg: '1/4 Initializing WASM Engine...' });
         const zsignModule = await createZSignModule();
 
         self.postMessage({ type: 'status', msg: '2/4 Loading files to Virtual FS...' });
@@ -22,17 +21,20 @@ self.onmessage = async function(e) {
         self.postMessage({ type: 'status', msg: '4/4 Extracting signed file...' });
         const signedIpaData = zsignModule.FS.readFile('signed.ipa');
 
-        // Отправляем готовый файл обратно в главный поток
-        self.postMessage({
-            type: 'done',
-            data: signedIpaData.buffer
-        }, [signedIpaData.buffer]);
+        // ВАЖНО: Копируем данные из памяти WASM, чтобы безопасно передать их
+        const safeData = signedIpaData.slice();
 
-        // Очищаем виртуалку
+        // Очищаем виртуалку ДО отправки файла, чтобы освободить оперативку
         zsignModule.FS.unlink('app.ipa');
         zsignModule.FS.unlink('cert.p12');
         zsignModule.FS.unlink('prov.mobileprovision');
         zsignModule.FS.unlink('signed.ipa');
+
+        // Отправляем готовый файл
+        self.postMessage({
+            type: 'done',
+            data: safeData.buffer
+        }, [safeData.buffer]); // Transferable object
 
     } catch (error) {
         self.postMessage({ type: 'error', msg: error.toString() });
